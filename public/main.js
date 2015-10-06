389451659782c6839744364727ef2cd96b63eb11
Jesse Jurman <j.r.jurman@gmail.com>
2015-10-06 16:43:58 -0400

    final version

---
+++ public/main.js
@@ -0,0 +1,67 @@
+// setup localStorage for client side data
+localStorage.clicked = localStorage.clicked || "[]";
+
+// click function to change color of a bingo square
+var clickSquare = function(id, color, reMark) {
+  if (color == undefined) {
+    color = "#3E6D57";
+  }
+  if (id == undefined) {
+    id = event.target.id;
+  }
+  console.log(id);
+  document.getElementById(id).style.backgroundColor = color;
+
+  if (reMark != false) {
+    // turn localstorage from string to javascript object
+    localStorageClicked = JSON.parse(localStorage.clicked);
+
+    // push our id into localstorage
+    localStorageClicked.push(id);
+
+    // save the id array back into localstorage as a string
+    localStorage.clicked = JSON.stringify(localStorageClicked);
+
+    // mark the square on the server
+    var markRequest = new XMLHttpRequest();
+    markRequest.open('GET', 'http://localhost:3000/mark/'+id);
+    markRequest.onload = function() {
+        if (markRequest.status === 200) {
+          console.log("marked position")
+        }
+        else {
+          // request failed
+        }
+    };
+    markRequest.send();
+  }
+};
+
+// apply click function to all bingo squares
+document.getElementsByClassName("bingo-square").click = clickSquare
+
+// turn localstorage from string to javascript object
+localStorageClicked = JSON.parse(localStorage.clicked);
+
+// click all the items that we've clicked in previous sessions
+localStorageClicked.forEach(function(id) {
+  clickSquare(id);
+});
+
+// click all the items that others have clicked
+// mark the square on the server
+var getRequest = new XMLHttpRequest();
+getRequest.open('GET', 'http://localhost:3000/get/');
+getRequest.onload = function() {
+    if (getRequest.status === 200) {
+      boardString = getRequest.responseText;
+      board = JSON.parse(boardString);
+      Object.keys(board).forEach(function(id) {
+        clickSquare(id, "#4C2B50", false)
+      });
+    }
+    else {
+      // request failed
+    }
+};
+getRequest.send();
